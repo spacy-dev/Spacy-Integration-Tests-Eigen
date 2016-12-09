124f0d918c1d3212542fa679588bc0d66e35fb7c
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-12-09 08:59:20 +0100

    Integration tests for Eigen

---
+++ Eigen/nonlinear_example_with_FunG.cpp
@@ -0,0 +1,42 @@
+#include <gtest.hh>
+
+#include <Eigen/Dense>
+#include <Spacy/spacy.h>
+#include <Spacy/Adapter/eigen.hh>
+#include <fung/fung.hh>
+
+const constexpr int dim = 2;
+using Vector = Eigen::VectorXd;
+using Matrix = Eigen::MatrixXd;
+
+auto get_fung_operator()
+{
+    Matrix A(dim,dim);
+    A.fill(0);
+    A(0,0) = 1;
+    A(1,1) = 2;
+
+    Vector b(dim);
+    b(0) = -10;
+    b(1) = -20;
+
+    // use FunG to generate operator
+    auto v = FunG::variable<0>(Vector(dim));
+    return (1+dot(v,A*v))*v + b;
+}
+
+TEST(SpacyEigenFunGIntegration,Nonlinear2DExample)
+{
+    using namespace Spacy;
+
+    auto A_ = get_fung_operator();
+    auto V = Rn::makeHilbertSpace(dim);
+    auto A = Rn::getFunGC1Operator(A_, V, V.dualSpace());
+    V.setScalarProduct( InducedScalarProduct( A.linearization(zero(V)) ) );
+
+    auto x = localNewton(A);
+
+    const auto& x_ = get(cast_ref<Rn::Vector>(x));
+    EXPECT_DOUBLE_EQ( x_(0), 1.0 );
+    EXPECT_DOUBLE_EQ( x_(1), 2.0 );
+}
---
+++ Eigen/util.cpp
@@ -0,0 +1,31 @@
+#include <gtest.hh>
+
+#include <Spacy/Adapter/Eigen/util.hh>
+#include <Spacy/Adapter/Eigen/vectorCreator.hh>
+
+#include <mockSetup.hh>
+
+#include "setup.hh"
+
+using namespace Spacy;
+
+TEST(Rn,GetSize)
+{
+  auto V = Rn::makeHilbertSpace(testDim());
+  auto x = zero(V);
+  ASSERT_EQ( Rn::getSize(x) , testDim() );
+}
+
+TEST(Rn,GetSizeOfProductSpace)
+{
+  auto V = Rn::makeHilbertSpace( {testDim(),testDim(),testDim()} );
+  auto x = zero(V);
+  ASSERT_EQ( Rn::getSize(x) , 3*testDim() );
+}
+
+TEST(Rn,GetSizeOfPrimalDualProductSpace)
+{
+  auto V = Rn::makeHilbertSpace( {testDim(),testDim(),testDim()} , {1,2} , {0} );
+  auto x = zero(V);
+  ASSERT_EQ( Rn::getSize(x) , 3*testDim() );
+}
